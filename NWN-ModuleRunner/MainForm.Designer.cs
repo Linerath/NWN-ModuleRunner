08f03b05b3f1406321d14e0361bd8299dca82a4e
Linerath <linerath@mail.ru>
2019-10-24 16:29:49 +0300

    Added basic functions (selecting coordinates and performing click)

---
+++ NWN-ModuleRunner/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace NWN_ModuleRunner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.X0 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Y0 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.X0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Start
+            // 
+            this.Btn_Start.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Btn_Start.Location = new System.Drawing.Point(156, 7);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(94, 56);
+            this.Btn_Start.TabIndex = 0;
+            this.Btn_Start.Text = "Start";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            // 
+            // X0
+            // 
+            this.X0.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.X0.Location = new System.Drawing.Point(37, 7);
+            this.X0.Name = "X0";
+            this.X0.Size = new System.Drawing.Size(84, 25);
+            this.X0.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y:";
+            // 
+            // Y0
+            // 
+            this.Y0.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Y0.Location = new System.Drawing.Point(37, 38);
+            this.Y0.Name = "Y0";
+            this.Y0.Size = new System.Drawing.Size(84, 25);
+            this.Y0.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 77);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Y0);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.X0);
+            this.Controls.Add(this.Btn_Start);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NWN Module runner";
+            ((System.ComponentModel.ISupportInitialize)(this.X0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y0)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.NumericUpDown X0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown Y0;
+    }
+}
+
---
+++ NWN-ModuleRunner/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using NWN_ModuleRunner.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NWN_ModuleRunner
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("User32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitParams();
+        }
+
+        private void Btn_Start_Click(object sender, EventArgs e)
+        {
+            if (AreCoordinatesValid)
+                PerformClick((int)X0.Value, (int)Y0.Value);
+        }
+
+        private void InitParams()
+        {
+            int w = Screen.PrimaryScreen.Bounds.Width;
+            int h = Screen.PrimaryScreen.Bounds.Height;
+
+            X0.Minimum = 0;
+            X0.Maximum = w;
+            Y0.Minimum = 0;
+            Y0.Maximum = h;
+
+            X0.Value = w / 2;
+            Y0.Value = h / 2;
+        }
+
+        private bool AreCoordinatesValid
+        {
+            get
+            {
+                int w = Screen.PrimaryScreen.Bounds.Width;
+                int h = Screen.PrimaryScreen.Bounds.Height;
+
+                return X0.Value >= 0 && X0.Value <= w
+                    && Y0.Value >= 0 && Y0.Value <= h;
+            }
+        }
+
+        #region Mouse
+        private void PerformClick()
+        {
+            mouse_event(0x0002 | 0x0004, 0, 0, 0, IntPtr.Zero);
+        }
+
+        private void PerformClick(int x, int y)
+        {
+            Cursor.Position = new Point(x, y);
+            PerformClick();
+        }
+        #endregion
+    }
+}
