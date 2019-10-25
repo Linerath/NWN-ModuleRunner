3b9a6b584112c932cc722c407c1e985c1422d7e1
Linerath <linerath@yandex.ru>
2019-10-25 10:53:30 +0300

    Added feature that allows setting XY from current cursor position

--- NWN-ModuleRunner/MainForm.Designer.cs
+++ NWN-ModuleRunner/MainForm.Designer.cs
@@ -35,6 +35,9 @@
             this.Y0 = new System.Windows.Forms.NumericUpDown();
             this.Cmb_Screens = new System.Windows.Forms.ComboBox();
             this.Lbl_Screen = new System.Windows.Forms.Label();
+            this.Lbl_Cursor = new System.Windows.Forms.Label();
+            this.Lbl_CursorXY = new System.Windows.Forms.Label();
+            this.Lbl_Hint0 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.X0)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Y0)).BeginInit();
             this.SuspendLayout();
@@ -42,7 +45,7 @@
             // Btn_Start
             // 
             this.Btn_Start.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Btn_Start.Location = new System.Drawing.Point(156, 55);
+            this.Btn_Start.Location = new System.Drawing.Point(186, 55);
             this.Btn_Start.Name = "Btn_Start";
             this.Btn_Start.Size = new System.Drawing.Size(94, 56);
             this.Btn_Start.TabIndex = 0;
@@ -53,7 +56,7 @@
             // X0
             // 
             this.X0.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.X0.Location = new System.Drawing.Point(37, 55);
+            this.X0.Location = new System.Drawing.Point(96, 55);
             this.X0.Name = "X0";
             this.X0.Size = new System.Drawing.Size(84, 25);
             this.X0.TabIndex = 1;
@@ -62,7 +65,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Location = new System.Drawing.Point(71, 57);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(19, 17);
             this.label1.TabIndex = 2;
@@ -72,7 +75,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Location = new System.Drawing.Point(71, 88);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(18, 17);
             this.label2.TabIndex = 4;
@@ -81,7 +84,7 @@
             // Y0
             // 
             this.Y0.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Y0.Location = new System.Drawing.Point(37, 86);
+            this.Y0.Location = new System.Drawing.Point(96, 86);
             this.Y0.Name = "Y0";
             this.Y0.Size = new System.Drawing.Size(84, 25);
             this.Y0.TabIndex = 3;
@@ -105,11 +108,46 @@
             this.Lbl_Screen.TabIndex = 6;
             this.Lbl_Screen.Text = "Screen:";
             // 
+            // Lbl_Cursor
+            // 
+            this.Lbl_Cursor.AutoSize = true;
+            this.Lbl_Cursor.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Lbl_Cursor.Location = new System.Drawing.Point(148, 9);
+            this.Lbl_Cursor.Name = "Lbl_Cursor";
+            this.Lbl_Cursor.Size = new System.Drawing.Size(50, 17);
+            this.Lbl_Cursor.TabIndex = 7;
+            this.Lbl_Cursor.Text = "Cursor:";
+            this.Lbl_Cursor.Visible = false;
+            // 
+            // Lbl_CursorXY
+            // 
+            this.Lbl_CursorXY.AutoSize = true;
+            this.Lbl_CursorXY.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Lbl_CursorXY.Location = new System.Drawing.Point(201, 9);
+            this.Lbl_CursorXY.Name = "Lbl_CursorXY";
+            this.Lbl_CursorXY.Size = new System.Drawing.Size(23, 17);
+            this.Lbl_CursorXY.TabIndex = 8;
+            this.Lbl_CursorXY.Text = "XY";
+            this.Lbl_CursorXY.Visible = false;
+            // 
+            // Lbl_Hint0
+            // 
+            this.Lbl_Hint0.AutoSize = true;
+            this.Lbl_Hint0.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Lbl_Hint0.Location = new System.Drawing.Point(12, 124);
+            this.Lbl_Hint0.Name = "Lbl_Hint0";
+            this.Lbl_Hint0.Size = new System.Drawing.Size(345, 17);
+            this.Lbl_Hint0.TabIndex = 9;
+            this.Lbl_Hint0.Text = "Press \"F1\" to set coordinates from current cursor position";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(262, 123);
+            this.ClientSize = new System.Drawing.Size(367, 151);
+            this.Controls.Add(this.Lbl_Hint0);
+            this.Controls.Add(this.Lbl_CursorXY);
+            this.Controls.Add(this.Lbl_Cursor);
             this.Controls.Add(this.Lbl_Screen);
             this.Controls.Add(this.Cmb_Screens);
             this.Controls.Add(this.label2);
@@ -121,6 +159,7 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NWN Module runner";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.X0)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Y0)).EndInit();
             this.ResumeLayout(false);
@@ -137,6 +176,9 @@
         private System.Windows.Forms.NumericUpDown Y0;
         private System.Windows.Forms.ComboBox Cmb_Screens;
         private System.Windows.Forms.Label Lbl_Screen;
+        private System.Windows.Forms.Label Lbl_Cursor;
+        private System.Windows.Forms.Label Lbl_CursorXY;
+        private System.Windows.Forms.Label Lbl_Hint0;
     }
 }
 
--- NWN-ModuleRunner/MainForm.cs
+++ NWN-ModuleRunner/MainForm.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,19 +15,14 @@ namespace NWN_ModuleRunner
 {
     public partial class MainForm : Form
     {
-        [DllImport("User32.dll")]
-        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
+        private IntPtr hookId = IntPtr.Zero;
 
         public MainForm()
         {
             InitializeComponent();
             InitParams();
-        }
 
-        private void Btn_Start_Click(object sender, EventArgs e)
-        {
-            if (AreParametersValid)
-                PerformClick((int)X0.Value, (int)Y0.Value);
+            hookId = PInvokeHelper.SetWindowsHookEx(PInvokeHelper.HookType.WH_KEYBOARD, KeyboardProc, IntPtr.Zero, AppDomain.GetCurrentThreadId());
         }
 
         private void InitParams()
@@ -36,9 +32,8 @@ namespace NWN_ModuleRunner
 
             if (Cmb_Screens.Items.Count < 1)
             {
-                MessageBox.Show("No screens are recognized.", "Error", MessageBoxButtons.OK);
+                MessageBox.Show("No screens are recognized.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Environment.Exit(0);
-                //Application.Exit();
             }
 
             Cmb_Screens.SelectedIndex = 0;
@@ -53,6 +48,36 @@ namespace NWN_ModuleRunner
 
             X0.Value = w / 2;
             Y0.Value = h / 2;
+        }
+
+        private int KeyboardProc(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code == 3)
+            {
+                #region K
+                Keys keyPressed = (Keys)wParam.ToInt32();
+
+                if (keyPressed == Keys.F1)
+                {
+                    if (Cursor.Position.X > X0.Maximum || Cursor.Position.Y > Y0.Maximum)
+                    {
+                        MessageBox.Show("Coordinates are out of boundaries", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        X0.Value = Cursor.Position.X;
+                        Y0.Value = Cursor.Position.Y;
+                    }
+                }
+                #endregion
+            }
+
+            return PInvokeHelper.CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
+        }
+
+        private void UpdateCursorPosition()
+        {
+            //Lbl_CursorXY.Text = $"{Cursor.Position.X};{Cursor.Position.Y}";
         }
 
         #region Form validity
@@ -88,7 +113,7 @@ namespace NWN_ModuleRunner
         #region Mouse
         private void PerformClick()
         {
-            mouse_event(0x0002 | 0x0004, 0, 0, 0, IntPtr.Zero);
+            PInvokeHelper.mouse_event(0x0002 | 0x0004, 0, 0, 0, IntPtr.Zero);
         }
 
         private void PerformClick(int x, int y)
@@ -98,5 +123,54 @@ namespace NWN_ModuleRunner
             PerformClick();
         }
         #endregion
+
+        #region Events
+        private void Btn_Start_Click(object sender, EventArgs e)
+        {
+            if (AreParametersValid)
+                PerformClick((int)X0.Value, (int)Y0.Value);
+            else
+                MessageBox.Show("One or more parameter is invalid.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (hookId.ToInt64() > 0)
+                PInvokeHelper.UnhookWindowsHookEx(hookId);
+        }
+        #endregion
+    }
+
+    internal static class PInvokeHelper
+    {
+        [DllImport("User32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
+        [DllImport("User32.dll")]
+        public static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc lpfn, IntPtr hMod, int dwThreadId);
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll")]
+        public static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        internal delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+        internal enum HookType
+        {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
     }
 }
