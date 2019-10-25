7904c289d7bc7b159741b30f834331f0ea71abfb
Linerath <linerath@yandex.ru>
2019-10-25 13:10:00 +0300

    Moved PInvokeHelper to the 'Services' folder

--- NWN-ModuleRunner/MainForm.cs
+++ NWN-ModuleRunner/MainForm.cs
@@ -174,37 +174,4 @@ namespace NWN_ModuleRunner
         }
         #endregion
     }
-
-    internal static class PInvokeHelper
-    {
-        [DllImport("User32.dll")]
-        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
-        [DllImport("User32.dll")]
-        public static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc lpfn, IntPtr hMod, int dwThreadId);
-        [DllImport("User32.dll", SetLastError = true)]
-        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
-        [DllImport("user32.dll")]
-        public static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        internal delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
-
-        internal enum HookType
-        {
-            WH_JOURNALRECORD = 0,
-            WH_JOURNALPLAYBACK = 1,
-            WH_KEYBOARD = 2,
-            WH_GETMESSAGE = 3,
-            WH_CALLWNDPROC = 4,
-            WH_CBT = 5,
-            WH_SYSMSGFILTER = 6,
-            WH_MOUSE = 7,
-            WH_HARDWARE = 8,
-            WH_DEBUG = 9,
-            WH_SHELL = 10,
-            WH_FOREGROUNDIDLE = 11,
-            WH_CALLWNDPROCRET = 12,
-            WH_KEYBOARD_LL = 13,
-            WH_MOUSE_LL = 14
-        }
-    }
 }
